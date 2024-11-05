0adf90aa933c5accea65c5674bf6c15d65da919c
SatomiJin <van.bui180903@gmail.com>
2024-11-05 21:23:17 +0700

    update avatar to chat message

--- src/components/NotesComponent/NoteItem/NoteItem.js
+++ src/components/NotesComponent/NoteItem/NoteItem.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import userAvatar from "../../../assets/images/user-image.png";
 import "./NoteItem.scss";
 import { useSelector } from "react-redux";
 function NoteItem(props) {
   let user = useSelector((state) => state.user);
   let [data, setData] = useState(props.data);
+
   const formatDate = (item) => {
     let date = new Date(item);
     let hours = String(date.getHours()).padStart(1, "0");
@@ -13,19 +15,29 @@ function NoteItem(props) {
     hours = hours ? String(hours).padStart(2, "0") : "12"; // Đặt lại giờ thành 12 nếu giờ bằng 0
 
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    // const seconds = String(date.getSeconds()).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0"); // Tháng bắt đầu từ 0
     const year = date.getFullYear();
     return `${hours}:${minutes} ${ampm} - ${day}/${month}/${year}`;
   };
+
   return (
     <div
       className="note-item_container"
       style={{ justifyContent: `${data && data.email === user?.email ? "end" : "start"}` }}
     >
       <div className="wrapper" style={{ textAlign: `${props.type === "right" ? "right" : "left"}` }}>
-        <div className="note_time">{formatDate(data.timeCreate)}</div>
+        <div
+          className="note_time"
+          style={{ flexDirection: `${data && data.email === user?.email ? "row-reverse" : "none"}` }}
+        >
+          <img
+            className="note_avatar"
+            src={`${data && data.author && data.author.image ? data.author.image : userAvatar}`}
+            alt="avatar-user"
+          />
+          {formatDate(data.timeCreate)}
+        </div>
         <div className="note_value">{data && data.email && data.messageNote}</div>
       </div>
     </div>
